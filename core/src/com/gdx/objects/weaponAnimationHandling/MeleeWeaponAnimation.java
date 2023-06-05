71472a73848c5e7c820b91f46505943fa26ddded
asus <c14220246@john.petra.ac.id>
2023-06-05 22:36:31 +0700

    update proyek

--- core/src/com/gdx/objects/weaponAnimationHandling/MeleeWeaponAnimation.java
+++ core/src/com/gdx/objects/weaponAnimationHandling/MeleeWeaponAnimation.java
@@ -22,7 +22,7 @@ public class MeleeWeaponAnimation implements WeaponAnimation{
             fixY = Gdx.input.getY();
         }
         float angle = getAngleToMouse(fixX, fixY, (float) (player.getHitBox().getX() + (player.getHitBox().width / 2.0f)), (float) (player.getHitBox().getY() + (player.getHitBox().height / 2.0f)));
-        batch.draw(weapon.get(0), (float) (player.getHitBox().getX() + (player.getHitBox().width / 2.0f)), (float) (player.getHitBox().getY() + (player.getHitBox().height / 2.0f)), 8, 0, 16, 46, sizeScaling, sizeScaling, (240 - angle) + (frame * 8));
+        batch.draw(weapon.get(0), (float) (player.getHitBox().getX() + (player.getHitBox().width / 2.0f)), (float) (player.getHitBox().getY() + (player.getHitBox().height / 2.0f)), 8, 0, 16, 46, sizeScaling, sizeScaling, (210 - angle) + (frame * 14));
         if (frame == getMaxFrame()-1){
             isAttacking = false;
         }
