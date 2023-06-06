69a626ffe2d1d74ebc662dbe5da6aa9eb18877b9
asus <c14220246@john.petra.ac.id>
2023-06-06 12:54:05 +0700

    update proyek

--- core/src/com/gdx/objects/weaponAnimationHandling/Explotion.java
+++ core/src/com/gdx/objects/weaponAnimationHandling/Explotion.java
@@ -4,36 +4,42 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Circle;
 
 import java.util.ArrayList;
 
 public class Explotion {
     int frame = 0;
-    Texture texture = new Texture("Vortex/Effect_TheVortex_1_427x431.png");
+    Circle circle;
+    Texture texture = new Texture("Vortex/Effect_TheVortex_1_429x429.png");
     ArrayList<TextureRegion> frames = new ArrayList<>();
 
     public Explotion() {
+        circle = new Circle();
+        circle.radius = 0;
         int X = 0;
         int Y = 0;
-        for (int i = 0; i < 30; i++) {
-            frames.add(new TextureRegion(texture,X,Y,427,431));
-            X+=427;
-            if (X > 2562){
+        for (int i = 0; i < 60; i++) {
+            frames.add(new TextureRegion(texture,X,Y,429,429));
+            X+=429;
+            if (X > 3861){
                 X = 0;
-                Y += 427;
-            }
-            if (Y > 2155){
-                Y = 0;
+                Y += 429;
             }
         }
     }
     public void draw(int X, int Y, Batch batch){
-        float centerX = X - (427 * 0.2f) / 2;
-        float centerY = Gdx.graphics.getHeight() - Y - (431 * 0.2f) / 2;
-        batch.draw(frames.get(frame), centerX,centerY,0,0,427,431,0.2f,0.2f,0);
+        if (circle.radius == 0){
+            circle.radius = 214 * 0.2f;
+            circle.setPosition(X,Y);
+        }
+        float centerX = X - (429 * 0.2f) / 2;
+        float centerY = Gdx.graphics.getHeight() - Y - (429 * 0.2f) / 2;
+        batch.draw(frames.get(frame), centerX,centerY,0,0,429,429,0.2f,0.2f,0);
         frame++;
-        if (frame == 30){
+        if (frame == 60){
             frame = 0;
+            circle.radius = 0;
         }
     }
 }
